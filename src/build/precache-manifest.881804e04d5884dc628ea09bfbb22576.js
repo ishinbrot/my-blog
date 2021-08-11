bf1ffa238185a4fd3f6dab44eb96b05fb6d2008f
Ian Shinbrot <ianshinbro@gmail.com>
2021-08-11 10:28:29 -0400

    First Commit

---
+++ src/build/precache-manifest.881804e04d5884dc628ea09bfbb22576.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c721266d5f21dd6e86ac",
+    "url": "/static/css/main.751d9a08.chunk.css"
+  },
+  {
+    "revision": "c721266d5f21dd6e86ac",
+    "url": "/static/js/main.a47de394.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7ffdd91f1cff151df4a2",
+    "url": "/static/js/2.a4654dbd.chunk.js"
+  },
+  {
+    "revision": "06f54b1bb933e54384714f4dea80e52d",
+    "url": "/index.html"
+  }
+];
